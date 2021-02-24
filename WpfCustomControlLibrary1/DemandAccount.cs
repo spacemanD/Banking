ca7e9641880c37f3cde00d75188d17f2770a046c
SpacemanD <dimasforus@gmail.com>
2021-02-24 20:35:35 +0200

    Add project files.

---
+++ WpfCustomControlLibrary1/DemandAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfCustomControlLibrary1
+{
+    public class DemandAccount : Account
+    {
+        public DemandAccount(decimal sum, int percentage) : base(sum, percentage)
+        {
+        }
+
+        protected internal override void Open()
+        {
+            base.OnOpened(new AccountEventArgs($"Открыт новый счет до востребования! Id счета: {this.Id}", this.Sum));
+        }
+    }
+}
---
+++ WpfCustomControlLibrary1/IAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfCustomControlLibrary1
+{
+    public interface IAccount
+    {
+        // Положить деньги на счет
+        void Put(decimal sum);
+        // Взять со счета
+        decimal Withdraw(decimal sum);
+    }
+}
